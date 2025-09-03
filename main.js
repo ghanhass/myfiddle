2a6dee7473b76c2f5c9fc677219210f830ed83d8
Hassen Ghanmi <hassoon@BULLSEYE-debian-live-builder>
2025-09-03 14:11:05 +0100

    new layour fixes

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 // src/main.ts
-import("./bootstrap-4ZCG7KLT.js").catch((err) => console.error(err));
+import("./bootstrap-A76IWJJZ.js").catch((err) => console.error(err));
 //# sourceMappingURL=main.js.map
